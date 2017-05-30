401fe88c082dcf9fb5df6c32ef7036e8758ea146
Savchenko Andrei <sava777@gmail.com>
2017-05-30 13:04:24 +0300

    Backup

---
+++ backup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+BACKUPDIR=backup
+NOW=$(date +%Y-%m-%d %H:%M:%S)
+
+if [ ! -d "$BACKUPDIR" ]; then
+  /bin/mkdir $BACKUPDIR
+fi
+
+while read srcdir dstdir
+do
+    if [ "$src" == "#" ]; then
+        continue 2
+    fi
+
+    if [ ! -d "$BACKUPDIR/$dstdir" ]; then
+        /bin/mkdir $BACKUPDIR/$dstdir
+    fi
+    /usr/bin/rsync -av --delete $srcdir $BACKUPDIR/$dstdir
+
+done < backup.conf
+
+/usr/bin/git add .
+/usr/bin/git commit -v -m "Backup $NOW"
+/usr/bin/git origin master
